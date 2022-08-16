2a7c605d3b133fe147ef5384b1cd8ce0e2c98db2
Pham Thanh Nguyen <nguyen.phamthanh@paradox.ai>
2022-08-16 16:50:45 +0700

    Implement header and footer 16.08.2022

---
+++ frontend/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="app">
+    <header>
+		<div class="header-bar">
+			<div class="header-title">Jin F4Shop</div>
+		</div>
+		<div class="header-container">
+			<div class="nav-container">
+				<div class="navigation">
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">Meet the Team</a></li>
+						<li><a href="#">FAQ</a></li>
+						<li><a href="#">Support</a></li>
+						<li><a href="#">Store Policies</a></li>
+						<li><a href="#">Testimonials</a></li>
+						<li><a href="#">Shop</a></li>
+					</ul>
+				</div>
+				<i class="fa fa-facebook"></i>
+			</div>
+		</div>
+	</header>
+  <main>
+	<div class="content-wrapper">
+		<div class="content-container">
+			<div class="img-introduce">
+			</div>
+			<div class="intro-container">
+				<div class="intro-title">CHECK US OUT</div>
+				<div class="intro-word">Save More</div>
+			</div>
+		</div>
+	</div>
+	<home-page :products="products"></home-page>
+  </main>
+  <footer>
+	<div class="subscribe-form">
+            <div class="form-title">Subscribe Form</div>
+            <div class="form-sub">
+                <input type="text" placeholder="e.g.email@gmail.com">
+                <button class="join-btn"><span>Join</span></button>
+            </div>
+    </div>
+	<div class="contact-info">
+		<div class="contact-mail">nguyen13901@gmail.com</div>
+		<div class="fb-icon"><i class="fa fa-facebook"></i></div>
+		<div class="copyright">&copy;2022 by ClothesShop. Proudly created with Wix.com</div>
+	</div>
+  </footer>
+</div>
+</template>
+
+<script>
+import HomePageVue from './components/HomePage.vue';
+export default {
+	name: "App",
+	components: {
+		'home-page': HomePageVue
+	},
+	data() {
+		return {
+			products: [
+				{name: "Banana", price: "20"},
+				{name: "Apple", price: "60"},
+				{name: "Watermelon", price: "10"},
+				{name: "Strawberry", price: "100"}
+			]
+		}
+	}
+};
+</script>
+
+<style>
+	@import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
+	@import "css/reset.css";
+	@import "css/main.css";
+</style>
---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="home">
+    <h2>List product one: </h2>
+    <ul>
+        <li v-for="product in products" :key="product">
+            <span class="name">Name: {{ product.name }}</span>
+            <span class="price">Price: {{ product.price }}</span>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['products'],
+    data() {
+        return {
+			
+        }
+    }
+}
+</script>
+<style scoped>
+#home{
+    background: #FFF8B1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+}
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
+</style>
