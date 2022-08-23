9007a297f1b77d682f7f6142d6e6fd7f7118bb2f
ThanhNguyen1392001 <nguyen13701@gmail.com>
2022-08-23 13:11:47 +0700

    Implement contact page 23.08.2022

---
+++ frontend/src/components/ContactPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="contact-page">
+        <contact-info></contact-info>
+        <branch-info></branch-info>
+    </div>
+</template>
+
+<script>
+import ContactInfo from '../pages/contact_page/ContactInfo.vue';
+import BranchInfo from '../pages/contact_page/BranchInfo.vue';
+
+export default {
+    components: {
+        'ContactInfo': ContactInfo,
+        'BranchInfo': BranchInfo,
+    }
+}
+</script>
+<style scoped>
+@import "../css/contact.css";
+</style>
---
+++ frontend/src/pages/contact_page/BranchInfo.vue
@@ -0,0 +1,26 @@
+<template>
+<div id="branch-info">
+    <h3 class="branch-tilte">ClothesShop Store</h3>
+    <p class="branch-short-desc">Buying Our Goods</p>
+    <div class="detail-desc-container">
+        <div class="detail-desc">
+            <p class="store-name">Store 1</p>
+            <p class="store-street">50 Nguyen Luong Bang street</p>
+            <p class="store-address"> Hoa Khanh Bac, Lien Chieu, Da Nang</p>
+        </div>
+        <div class="detail-desc">
+            <p class="store-name">Store 2</p>
+            <p class="store-street">273 Phan Chau Trinh street</p>
+            <p class="store-address"> Hai Chau, Da Nang</p>
+        </div>
+        <div class="detail-desc">
+            <p class="store-name">Store 3</p>
+            <p class="store-street">139 Pham Ngu Lao street</p>
+            <p class="store-address"> Quan 1, Ho Chi Minh</p>
+        </div>
+        <div class="detail-desc">
+            <p>Contact</p>
+        </div>
+    </div>
+</div>
+</template>
